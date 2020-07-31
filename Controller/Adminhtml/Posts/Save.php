22774d3c166bd0b4b6450ea09bd2e90c2ba854c2
leduydong94 <leduydong281@gmail.com>
2020-07-31 10:01:54 +0700

    "update save"

--- Controller/Adminhtml/Posts/Save.php
+++ Controller/Adminhtml/Posts/Save.php
@@ -1,8 +1,8 @@
 <?php
 namespace Sample\Helloworld\Controller\Adminhtml\Posts;
- 
+
 use Sample\Helloworld\Controller\Adminhtml\Posts;
- 
+
 class Save extends Posts
 {
     /**
@@ -11,37 +11,40 @@ class Save extends Posts
     public function execute()
     {
         $isPost = $this->getRequest()->getPost();
- 
+
         if ($isPost) {
             $postsModel = $this->_postsFactory->create();
             $postsId = $this->getRequest()->getParam('id');
- 
+
             if ($postsId) {
                 $postsModel->load($postsId);
             }
             $formData = $this->getRequest()->getParam('post');
+            $create_at = date("Y-m-d H:i:s");
+            $formData['create_at'] = $create_at;
+//            die($formData);
+
             $postsModel->setData($formData);
- 
             try {
                 // Save news
                 $postsModel->save();
- 
+
                 // Display success message
                 $this->messageManager->addSuccess(__('The news has been saved.'));
- 
+
                 // Check if 'Save and Continue'
                 if ($this->getRequest()->getParam('back')) {
                     $this->_redirect('*/*/edit', ['id' => $postsModel->getId(), '_current' => true]);
                     return;
                 }
- 
+
                 // Go to grid page
                 $this->_redirect('*/*/');
                 return;
             } catch (\Exception $e) {
                 $this->messageManager->addError($e->getMessage());
             }
- 
+
             $this->_getSession()->setFormData($formData);
             $this->_redirect('*/*/edit', ['id' => $postsId]);
         }
